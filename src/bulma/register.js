fc3dcbdc3d6b6ae06a76984bafdf261038140fc4
reza <raftx24@gmail.com>
2020-11-12 13:59:55 +0330

    adds ThemeSelector.vue

---
+++ src/bulma/register.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import App from '@enso-ui/ui/src/core/app';
+import ThemeSelector from './components/settings/ThemeSelector.vue';
+
+Vue.component('setting-theme-selector', ThemeSelector);
+App.registerSettingsItem('setting-theme-selector', 10);
