b8f4db597210828da9afde3a393cf298f04fc15d
Mihai <mihai.ocneanu@gmail.com>
2021-12-17 17:05:44 +0200

    wip

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -1,6 +1,4 @@
-import Vue from 'vue';
 import App from '@enso-ui/ui/src/core/app';
 import ThemeSelector from './components/settings/ThemeSelector.vue';
 
-Vue.component('setting-theme-selector', ThemeSelector);
-App.registerSettingsItem('setting-theme-selector', 200);
+App.registerSettingsItem('setting-theme-selector', ThemeSelector, 200);
